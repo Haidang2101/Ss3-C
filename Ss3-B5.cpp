f313582164fdd14b5124a4213baa8c070ba56d49
Haidang2101 <haidanghanoi2003@gmail.com>
2024-11-22 00:29:33 +0700

    B5

---
+++ Ss3-B5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("Danh sach sinh vien:\n");
+    printf("-------------------------------------------------------------\n");
+    printf("STT   Ten              Tuoi   So DT         Email\n");
+    printf("-------------------------------------------------------------\n");
+    printf("1     Nguyen Van A     20     0123456789    nva@gmail.com\n");
+    printf("2     Nguyen Van B     21     0987654321    ttb@gmail.com\n");
+    printf("3     Nguyen Van C     19     0901234567    lvc@gmail.com\n");
+    printf("4     Nguyen Van D     21     0974229045    ptd@gmail.com\n");
+    printf("5     Nguyen Van E     18     0976589645    ptd@gmail.com\n");
+    printf("6     Nguyen Van F     22     0912345678    ptd@gmail.com\n");
+    printf("-------------------------------------------------------------\n");
+
+    return 0;
+}
